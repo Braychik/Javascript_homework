be476a0922c57efe008041a88a34cd73fcd8334b
Николай <ibraevnikolay@mail.ru>
2024-04-30 17:46:27 +0500

    homework 4 in ecma

--- ECMA_Sem_4/homework.js
+++ ECMA_Sem_4/homework.js
@@ -5,18 +5,18 @@
 // Подсказка, с последовательностью действий:
 // getUserData использует fetch для получения данных о пользователе с удаленного сервера. Если запрос успешен (с кодом 200), функция извлекает данные из ответа с помощью response.json() и возвращает объект с данными о пользователе. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
-// async function getUserData(ID) {
-//     const responce = await fetch(`https://jsonplaceholder.typicode.com/users`);
-//     const users = await responce.json();
-//     if (responce.status !== 200) {
-//       console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-//     } else {
-//       const user = users.find((el) => el.id === ID);
-//       console.log(user === undefined ? "Пользователь не найден" : user);
-//     }
-//   }
-//   getUserData(1);
-//   getUserData(11);
+async function getUserData(ID) {
+    const responce = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const users = await responce.json();
+    if (responce.status !== 200) {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      const user = users.find((el) => el.id === ID);
+      console.log(user === undefined ? "Пользователь не найден" : user);
+    }
+  }
+  getUserData(1);
+  getUserData(11);
 
 //   ""Отправка данных на сервер""
 
@@ -40,44 +40,44 @@
 
 //   saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 200), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщени
 
-// async function saveUserData(user, url) {
-//     let response = await fetch(url, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(user),
-//     });
-//     if (!response.ok) {
-//       console.log(response.status);
-//       throw new Error("Что-то пошло не так: " + response.status);
-//     } else {
-//       console.log(response.status);
-//     }
-//   }
-//   const user = {
-//     name: "John Smith",
-//     age: 30,
-//     email: "john@example.com",
-//   };
-//   const url1 = "https://jsonplaceholder.typicode.com/us";
-//   const url2 = "https://jsonplaceholder.typicode.com/users";
+async function saveUserData(user, url) {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+      console.log(response.status);
+      throw new Error("Что-то пошло не так: " + response.status);
+    } else {
+      console.log(response.status);
+    }
+  }
+  const user = {
+    name: "John Smith",
+    age: 30,
+    email: "john@example.com",
+  };
+  const url1 = "https://jsonplaceholder.typicode.com/us";
+  const url2 = "https://jsonplaceholder.typicode.com/users";
 
-//   saveUserData(user, url1)
-//     .then(() => {
-//       console.log("User data saved successfully");
-//     })
-//     .catch((error) => {
-//       console.log(error.message);
-//     });
+  saveUserData(user, url1)
+    .then(() => {
+      console.log("User data saved successfully");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 
-//   saveUserData(user, url2)
-//     .then(() => {
-//       console.log("User data saved successfully");
-//     })
-//     .catch((error) => {
-//       console.log(error.message);
-//     });
+  saveUserData(user, url2)
+    .then(() => {
+      console.log("User data saved successfully");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 
 // ""Изменение стиля элемента через заданное время""
 
